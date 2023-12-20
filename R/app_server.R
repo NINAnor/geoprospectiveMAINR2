53d9e1c752d6f948de6f05f383d1493c01bd227b
Reto Spielhofer <reto.spielhofer@nina.no>
2023-12-20 08:54:25 +0100

    list structure of reactive values

--- R/app_server.R
+++ R/app_server.R
@@ -37,8 +37,6 @@ table_con<-data.frame(
   billing =project
 )
 
-# rgee::ee_Initialize(user = 'reto.spielhofer@nina.no')
-
 
 con <- dbConnect(
   bigrquery::bigquery(),
@@ -58,18 +56,13 @@ vis_ind <- list(min = 0, max = 1, palette = cols, values = labels)
 vis_cv <- list(min = 0, max = 0.5, palette = cols_cv)
 vis_diff <- list(min = -1, max = 1, palette = cols_diff)
 
-
-# rgee::ee_Initialize(user = 'reto.spielhofer@nina.no')
-
 app_server <- function(input, output, session) {
   # Your application server logic
-  
   hideTab(inputId = "inTabset", target = "p1")
 
   studies<-eventReactive(input$check_site,{
     study_site<-tbl(con, "study_site")
     studies<-study_site%>%select(siteID,siteTYPE,siteNMAPPING,siteCREATETIME,siteSTATUS)%>%collect()
-    # studies$siteCREATETIME<-as.POSIXct(studies$siteCREATETIME)
   })
   
   observeEvent(input$check_site,{
@@ -125,137 +118,121 @@ app_server <- function(input, output, session) {
   
   ###load study
 
-  observeEvent(input$load,{
-    rgee::ee_Initialize(user = 'reto.spielhofer@nina.no')
-  })
-  
-  user_id<-eventReactive(input$load,{
-    user_conf<-user_conf()
-    user_conf<-user_conf%>%filter(userMAIL == input$user_mail)
-    user_id<-as.character(user_conf$userID)
+  # observeEvent(input$load,{
+  #   rgee::ee_Initialize(user = 'reto.spielhofer@nina.no')
+  # })
+  param_list<-eventReactive(input$load,{
+    withProgress(message = "Connect to server",value = 0.2,{
+      rgee::ee_Initialize(user = 'reto.spielhofer@nina.no')
+      incProgress(amount = 0.4,message = "Collect your data")
+      user_conf<-user_conf()
+      req(studies)
+      studies<-studies()
+      
+      user_conf<-user_conf%>%filter(userMAIL == input$user_mail)
+      user_id<-as.character(user_conf$userID)
+      
+      
+      
+      mapping<-tbl(con, "es_mappingR1")
+      # join the rest of the es infos from the es_descr table
+      es_descr<-tbl(con,"es_descr")
+      # ind_es_map<-left_join(ind_es_map,es_descr,by="esID")
+      
+      userES<-mapping%>%select(userID, esID, siteID, poss_mapping, expert_trust)%>%
+        filter(userID==user_id)%>%
+        left_join(es_descr,by="esID")%>%collect()
+      userES<-as.data.frame(userES)
+      #shuffle rows randomly that not all the participants have the same order of mapping es
+      # userES<-userES[sample(nrow(userES)),]
+      incProgress(amount = 0.6,message = "download site geometries")
+      
+      
+      site_type<-as.character(studies%>%filter(siteID==input$site_id)%>%select(siteTYPE)%>%first())
+      
+      
+      assetid <- paste0('projects/eu-wendy/assets/study_sites/', input$site_id)
+      bound <- ee$FeatureCollection(assetid)
+      sf_bound<-ee_as_sf(bound)
+      
+      coords <- st_coordinates(sf_bound)
+      coords<-as.data.frame(coords[,c(1,2)])
+      
+      site_geom_ee<- paste0('projects/eu-wendy/assets/study_sites/', input$site_id)
+      site_geom_ee <- ee$FeatureCollection(site_geom_ee)
+      if(site_type == "onshore"){
+        # landcover
+        on_lulc <- ee$Image("COPERNICUS/CORINE/V20/100m/2018")
+        on_lulc<-on_lulc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=100)
+        on_lulc<-on_lulc$clip(site_geom_ee)$rename("on_lulc")
+        
+        #Hill et al., 2022: ecosystem integrity (structure, composition, and function against current actual potential as baseline) (0-1)
+        on_int<-"projects/eu-wendy/assets/ON_INT"
+        on_int<-ee$Image(on_int)
+        on_int<-on_int$clip(site_geom_ee)
+        on_int<-on_int$resample("bilinear")$reproject(crs= "EPSG:4326")
+        on_int<-on_int$rename("on_int")
+        
+        # global friction surface (land-based accessibility using motorized vehicles)
+        on_acc = ee$Image('Oxford/MAP/friction_surface_2019')$select("friction")
+        on_acc<-on_acc$resample("bilinear")$reproject(crs= "EPSG:4326")
+        on_acc<-on_acc$clip(site_geom_ee)$rename("on_acc")
+        
+        comb<-ee$Image$cat(on_lulc,on_int,on_acc)
+      }else{
+        
+        #bathymetry
+        off_bat = ee$Image('NOAA/NGDC/ETOPO1')$select('bedrock')
+        off_bat<-off_bat$resample("bilinear")$reproject(crs= "EPSG:4326")
+        off_bat<-off_bat$clip(site_geom_ee)
+        
+        ## lulc sea
+        off_lulc<-"projects/eu-wendy/assets/OFF_LULC"
+        off_lulc<-ee$Image(off_lulc)
+        off_lulc<-off_lulc$clip(site_geom_ee)
+        off_lulc<-off_lulc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
+        off_lulc<-off_lulc$rename("off_lulc")
+        
+        # dist coast
+        off_acc<-"projects/eu-wendy/assets/OFF_ACC"
+        off_acc<-ee$Image(off_acc)
+        off_acc<-off_acc$clip(site_geom_ee)
+        off_acc<-off_acc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
+        off_acc<-off_acc$rename("off_acc")
+        
+        #Human Impacts to Marine Ecosystems
+        off_nat<-"projects/eu-wendy/assets/OFF_NAT"
+        off_nat<-ee$Image(off_nat)
+        off_nat<-off_nat$clip(site_geom_ee)
+        off_nat<-off_nat$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
+        off_nat<-off_nat$rename("off_nat")
+        
+        comb<-ee$Image$cat(off_bat,off_lulc,off_acc,off_nat)
+      }
+      incProgress(amount = 0.8,message = "prepare geodata")
+      
+      if(site_type == "onshore"){
+        bands<-list("on_lulc","on_int","on_acc")
+      }else{
+        bands<-list("off_bat","off_lulc","off_ac","off_nat")
+      }
+      
+      
+      num_tabs<-as.integer(nrow(userES))
+      
+    })#/progress bar
     
-  })
-  
-  print("A")
-  
-  userES<-eventReactive(input$load,{
-    req(user_id)
-    user_id<-user_id()
-    mapping<-tbl(con, "es_mappingR1")
-    # join the rest of the es infos from the es_descr table
-    es_descr<-tbl(con,"es_descr")
-    # ind_es_map<-left_join(ind_es_map,es_descr,by="esID")
+    list(user_id, userES,site_type,sf_bound,coords,comb,bands,num_tabs)
     
-    userES<-mapping%>%select(userID, esID, siteID, poss_mapping, expert_trust)%>%
-      filter(userID==user_id)%>%
-      left_join(es_descr,by="esID")%>%collect()
-    userES<-as.data.frame(userES)
-    #shuffle rows randomly that not all the participants have the same order of mapping es
-    # userES<-userES[sample(nrow(userES)),]
-  })
-  
-  site_type<-eventReactive(input$load,{
-    req(studies)
-    studies<-studies()
-    site_type<-as.character(studies%>%filter(siteID==input$site_id)%>%select(siteTYPE)%>%first())
-  })
-  
+  })#/return list
 
-  sf_bound<-eventReactive(input$load,{
-    assetid <- paste0('projects/eu-wendy/assets/study_sites/', input$site_id)
-    bound <- ee$FeatureCollection(assetid)
-    sf_bound<-ee_as_sf(bound)
-  })
- 
-  coords<-eventReactive(input$load,{
-    req(sf_bound)
-    sf_bound<-sf_bound()
-    coords <- st_coordinates(sf_bound)
-    coords<-as.data.frame(coords[,c(1,2)])
-  })
-
-    comb<-eventReactive(input$load,{
-    req(site_type)
-    site_type<-site_type()
-    site_geom_ee<- paste0('projects/eu-wendy/assets/study_sites/', input$site_id)
-    site_geom_ee <- ee$FeatureCollection(site_geom_ee)
-    if(site_type == "onshore"){
-      # landcover
-      on_lulc <- ee$Image("COPERNICUS/CORINE/V20/100m/2018")
-      on_lulc<-on_lulc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=100)
-      on_lulc<-on_lulc$clip(site_geom_ee)$rename("on_lulc")
-      
-      #Hill et al., 2022: ecosystem integrity (structure, composition, and function against current actual potential as baseline) (0-1)
-      on_int<-"projects/eu-wendy/assets/ON_INT"
-      on_int<-ee$Image(on_int)
-      on_int<-on_int$clip(site_geom_ee)
-      on_int<-on_int$resample("bilinear")$reproject(crs= "EPSG:4326")
-      on_int<-on_int$rename("on_int")
-      
-      # global friction surface (land-based accessibility using motorized vehicles)
-      on_acc = ee$Image('Oxford/MAP/friction_surface_2019')$select("friction")
-      on_acc<-on_acc$resample("bilinear")$reproject(crs= "EPSG:4326")
-      on_acc<-on_acc$clip(site_geom_ee)$rename("on_acc")
-      
-      comb<-ee$Image$cat(on_lulc,on_int,on_acc)
-    }else{
-      
-      #bathymetry
-      off_bat = ee$Image('NOAA/NGDC/ETOPO1')$select('bedrock')
-      off_bat<-off_bat$resample("bilinear")$reproject(crs= "EPSG:4326")
-      off_bat<-off_bat$clip(site_geom_ee)
-      
-      ## lulc sea
-      off_lulc<-"projects/eu-wendy/assets/OFF_LULC"
-      off_lulc<-ee$Image(off_lulc)
-      off_lulc<-off_lulc$clip(site_geom_ee)
-      off_lulc<-off_lulc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
-      off_lulc<-off_lulc$rename("off_lulc")
-      
-      # dist coast
-      off_acc<-"projects/eu-wendy/assets/OFF_ACC"
-      off_acc<-ee$Image(off_acc)
-      off_acc<-off_acc$clip(site_geom_ee)
-      off_acc<-off_acc$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
-      off_acc<-off_acc$rename("off_acc")
-      
-      #Human Impacts to Marine Ecosystems
-      off_nat<-"projects/eu-wendy/assets/OFF_NAT"
-      off_nat<-ee$Image(off_nat)
-      off_nat<-off_nat$clip(site_geom_ee)
-      off_nat<-off_nat$resample("bilinear")$reproject(crs= "EPSG:4326",scale=1000)
-      off_nat<-off_nat$rename("off_nat")
-      
-      comb<-ee$Image$cat(off_bat,off_lulc,off_acc,off_nat)
-    }
-  })
-
-  bands<-eventReactive(input$load,{
-    req(site_type)
-    site_type<-site_type()
-    
-    if(site_type == "onshore"){
-      bands<-list("on_lulc","on_int","on_acc")
-    }else{
-      bands<-list("off_bat","off_lulc","off_ac","off_nat")
-    }
-    
-    
-  })
-  
-  num_tabs<-eventReactive(input$load,{
-    req(userES)
-    userES<-userES()
-    num_tabs<-as.integer(nrow(userES))
-  })
-  
 ### create N remapping tabs
   # print(num_tabs)
   observeEvent(input$load,{
-    req(num_tabs)
+    req(param_list)
     hideTab(inputId = "inTabset",
             target = "p0")
-    num_tabs<-num_tabs()
+    num_tabs<-param_list()[[8]]
     output$tabs <- renderUI({
       do.call(tabsetPanel, c(id="tabs_content",
                              lapply(1:num_tabs, function(i) {
@@ -271,14 +248,14 @@ app_server <- function(input, output, session) {
 
   ## hide tabs
   observeEvent(input$tabs_content, {
-    num_tabs<-num_tabs()
-    userES<-userES()
-    sf_bound<-sf_bound()
-    comb<-comb()
-    bands<-bands()
-    userID<-user_id()
-    site_type<-site_type()
-    coords<-coords()
+    num_tabs<-param_list()[[8]]
+    userES<-param_list()[[2]]
+    sf_bound<-param_list()[[4]]
+    comb<-param_list()[[6]]
+    bands<-param_list()[[7]]
+    userID<-param_list()[[1]]
+    site_type<-param_list()[[3]]
+    coords<-param_list()[[5]]
 
 
     for (i in 2:num_tabs) {
